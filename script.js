cb645e28debb3155ff20038d91c05836ee0b8a07
Yash Tripathi <rajattripathi1477@gmail.com>
2024-09-03 22:36:39 +0530

    added

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
-function todos(){
-    const todos = JSON.parse(localStorage.getItem("todos")) || []
+function loadTodos(){
+    const todos = JSON.parse(localStorage.getItem("todos")) || {"todoList" : []}
     console.log(todos)
+    return todos
+}
+
+function addTodoToLocalStorage(todoText){
+    const todos = loadTodos()
+    todos.todoList.push(todoText)
+    localStorage.setItem("todos" , JSON.stringify(todos))
 }
 
 document.addEventListener("DOMContentLoaded" , () => {
-    todos()
+let todoInput = document.getElementById("todoInput");
+let submitButton = document.getElementById("addTodo");
+
+submitButton.addEventListener("click" , (event) => {
+    const todoText = todoInput.value
+
+    if(todoText == ''){
+        alert("Empty String")
+    }
+    else{
+        addTodoToLocalStorage(todoText)
+    }
 })
+
+
+todoInput.addEventListener("change" , (event) => {
+    const todoText = event.target.value
+    event.target.value = todoText.trim()
+})
+
+
+    loadTodos()
+})
+
+console.log("yash")
